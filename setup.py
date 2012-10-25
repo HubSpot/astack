68265d195ce80b7836b080644e3179f5a2d051bb
Mike Axiak <maxiak@hubspot.com>
2012-10-25 15:29:19 -0400

    Increment version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='astack',
-    version='0.0.1',
+    version='0.0.2',
     description='Simple stacktrace analysis tool for the JVM',
     long_description=open('README.rst').read(),
     author='Michael Axiak',
