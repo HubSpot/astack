e3ae024184b11efd88351a0ad40e77871af5180e
Michael Axiak <maxiak@hubspot.com>
2014-05-02 13:18:24 -0400

    Fix the setup.py definition

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='astack',
-    version='0.0.3',
+    version='0.0.4',
     description='Simple stacktrace analysis tool for the JVM',
     long_description=open('README.markdown').read(),
     author='Michael Axiak',
